6122f5510dcc5c8aff7ade1f4b9bbadf165fa318
Avnit Bambah <abambah@gmail.com>
2015-04-03 12:48:49 -0500

    Adding my complete portfolio. Need another service for bonds :-(

--- src/main/groovy/startSelector/selector.groovy
+++ src/main/groovy/startSelector/selector.groovy
@@ -1,22 +1,43 @@
 package startSelector
 
 class selector {
-    public  static void  main(String[] args ) {
-         print "starting stock calculator \n"
-		 
-		 
-		 def finalPrice 
-		 def a = [ 'AAPL', 'GOOG', 'XOM']
-		 
-		 a.each {
-        def api = "http://download.finance.yahoo.com/d/quotes.csv?s=" + it + "&f=nsl1op&e=.csv"
-     
-        def uri_yahoo = new URL(api).getText()
-        finalPrice = finalPrice + uri_yahoo
-    //    WebserviceCall.yahooApi("GOOG")
-        
-         }
-		 print finalPrice
-    } 
+	public  static void  main(String[] args ) {
+		print "starting stock calculator \n"
 
+
+		def finalPrice
+		def a = [
+			'AAPL',
+			'GOOG',
+			'XOM' ,
+			'DIS',
+			'CMCSA',
+			'PG',
+			'KO',
+			'XOM',
+			'CVX',
+			'JPM',
+			'BRKB',
+			'WFC',
+			'PFE',
+			'JNJ',
+			'GE',
+			'MMM',
+			'DD',
+			'DOW',
+			'MSFT',
+			'VZ',
+			'DUK'
+		]
+
+		a.each {
+			def api = "http://download.finance.yahoo.com/d/quotes.csv?s=" + it + "&f=nsl1op&e=.csv"
+
+			def uri_yahoo = new URL(api).getText()
+			finalPrice = finalPrice + uri_yahoo
+			//    WebserviceCall.yahooApi("GOOG")
+
+		}
+		print finalPrice
+	}
 }
