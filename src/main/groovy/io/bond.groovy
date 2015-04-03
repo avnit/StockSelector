9aae00b4c06d9ef64960e6d34983e9dc75bc3e48
Avnit Bambah <abambah@gmail.com>
2015-04-03 12:20:40 -0500

    Added -bond.groovy -startselector/selector.groovy

---
+++ src/main/groovy/io/bond.groovy
@@ -0,0 +1,30 @@
+package io
+
+class bond {
+	double present_value
+	double face_value_of_bond
+	double price
+	double ytm
+	double interest_rate
+	double time_to_maturity
+	int frequency
+	int months_until_next_coupon
+	// calculate price
+	// if you need to calculate price then
+	public  calculatePresentValue()
+	{
+		//=(C4*C3/C5)*(1-(1+((1+C7)^(1/C5)-1))^(0-C6*C5))/((1+C7)^(1/C5)-1)+C3/(1+C7)^(C6+C9/12)+C3*C4/C5/(1+C7)^(C9/12)
+	   //Face Value of Bond = c3 = fv
+	   // Coupon Rate on Bond = c4 = rate
+		 //   Frequency of coupons = c5 = frequency
+			// Time to Maturity = c6 = term
+		  //  Interest Rate on Bond  = c7 = rate
+		  //  If seasoned bond, enter the following c8
+		// Months until next coupon = c9 =
+		present_value = (interest_rate * face_value_of_bond / frequency ) * ( 1 - 1 + ((1 + interest_rate)**(1/frequency) -1)) ** (0 - time_to_maturity*frequency)/(1+interest_rate)**(1/frequency) -1  + face_value_of_bond/(1+interest_rate)**(time_to_maturity + months_until_next_coupon/12 )
+				+face_value_of_bond * interest_rate / frequency /( 1+ interest_rate) ** (months_until_next_coupon/12)
+ 
+		print "computed present value of bond is ${present_value}"
+		
+	}
+}
---
+++ src/main/groovy/startSelector/selector.groovy
@@ -0,0 +1,10 @@
+package startSelector
+
+
+class selector {
+	public  static void  main(String[] args ) {
+		print "starting stock calculator"
+		
+	} 
+
+}
